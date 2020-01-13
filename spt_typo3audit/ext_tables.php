1e35eb48ae596f064de29075d761147ae908ace0
Arun Chandran <arun@spawoz.com>
2020-01-13 20:03:01 +0530

    TYPO3 Audit Initial Version

---
+++ spt_typo3audit/ext_tables.php
@@ -0,0 +1,34 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function()
+    {
+
+        if (TYPO3_MODE === 'BE') {
+
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'SPT.SptTypo3audit',
+                'tools', // Make module a submodule of 'tools'
+                'spttypo3audit', // Submodule key
+                '', // Position
+                [
+                    'Audit' => 'list',
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon'   => 'EXT:spt_typo3audit/Resources/Public/Icons/ext_icons.svg',
+                    'labels' => 'LLL:EXT:spt_typo3audit/Resources/Private/Language/locallang_spttypo3audit.xlf',
+                    'navigationComponentId' => '',
+                ]
+            );
+
+        }
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('spt_typo3audit', 'Configuration/TypoScript', 'TYPO3 Audit');
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_spttypo3audit_domain_model_audit', 'EXT:spt_typo3audit/Resources/Private/Language/locallang_csh_tx_spttypo3audit_domain_model_audit.xlf');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_spttypo3audit_domain_model_audit');
+
+    }
+);
