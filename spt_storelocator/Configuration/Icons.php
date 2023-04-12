642469d2cf18f2d194ff1fbb9d0915ac1c28e5fb
Arun Chandran <arun@spawoz.com>
2023-04-12 16:31:24 +0530

    Create Icons.php

---
+++ spt_storelocator/Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'spt_storelocator-plugin-list' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:spt_storelocator/Resources/Public/Icons/user_plugin_list.svg'
+    ],
+    'spt_storelocator-plugin-map' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:spt_storelocator/Resources/Public/Icons/user_plugin_map.svg'
+    ],
+];
