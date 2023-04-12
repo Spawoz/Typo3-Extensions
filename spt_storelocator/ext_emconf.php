9550515a4df094e70cd4e009b3932a6390ed0127
Arun Chandran <arun@spawoz.com>
2023-04-12 16:36:05 +0530

    Update ext_emconf.php

--- spt_storelocator/ext_emconf.php
+++ spt_storelocator/ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Store Locator (List/Map View)',
-    'description' => 'Extension to manage store location data.',
+    'description' => 'The Store Locator extension will help to add some basic store information in the backend and display it as a list or map view in the frontend.',
     'category' => 'plugin',
     'author' => 'Arun Chandran',
     'author_email' => 'arun@spawoz.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
